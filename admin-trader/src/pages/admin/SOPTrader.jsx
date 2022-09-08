14acf6450ebf9d19310b4a31432c18ba92026fd1
Estedio Technologies <estediotechnologies@gmail.com>
2022-09-08 19:34:17 +0700

    update

--- admin-trader/src/pages/admin/SOPTrader.jsx
+++ admin-trader/src/pages/admin/SOPTrader.jsx
@@ -1,51 +1,126 @@
 import React from "react";
 
 const SOPTrader = () => {
-  return (
-    <div>
-      <div className="row">
-        <div className="col-md-6">
-          <p className="nav-crumbs">Standard Operation Procedure</p>
+    return (
+        <div>
+            <div className="col-lg-12">
+                <p>
+                    <strong>SOP TRADER</strong>
+                </p>
+                <p>Tujuan:</p>
+                <ol>
+                    <li>
+                        Menyediakan standar dan arahan bagi <em>Trader</em>{" "}
+                        dalam menjalankan tugasnya sebagai <em>Trader</em>
+                    </li>
+                    <li>
+                        Mengoptimalkan kinerja <em>Trader</em> agar memberikan
+                        jasa terbaik bagi Perusahaan.
+                    </li>
+                </ol>
+                <p>Prosedur Kerja:</p>
+                <ol>
+                    <li>
+                        Setiap <em>Trader </em>melakukan absensi pada{" "}
+                        <em>Dashboard Trader</em> setiap memasuki jadwal{" "}
+                        <em>shift</em>-nya.
+                    </li>
+                    <li>
+                        Jadwal <em>shift Trader </em>dimulai dari pukul 08.00
+                        WIB sampai dengan pukul 08:00 WIB hari berikutnya.
+                        Detail pembagian jadwal <em>shift Trader</em> dapat
+                        dilihat pada menu Jadwal <em>Shift </em>pada{" "}
+                        <em>Dashboard Trader</em>.
+                    </li>
+                    <li>
+                        Setiap <em>Trader </em>dapat mengajukan pergantian
+                        tanggal <em>shift</em> kepada <em>Trader</em> lain
+                        dengan syarat <em>Trader </em>yang bersangkutan setuju
+                        untuk menggantikannya pada <em>shift </em>
+                    </li>
+                    <li>
+                        Setiap <em>Trader </em>melakukan <em>trading</em> pada{" "}
+                        <em>spot market</em> selama 24 jam, 7 hari.
+                    </li>
+                    <li>
+                        Dalam melakukan <em>trading </em>pada{" "}
+                        <em>spot market</em>, setiap <em>trader</em>:
+                        <ol>
+                            <li>
+                                Wajib melakukan analisa secara teknikal dan
+                                fundamental terhadap market aset kripto yang
+                                sedang diperdagangkan dan wajib mengetahui
+                                indikasi <em>bearish market</em>;
+                            </li>
+                            <li>
+                                Wajib memilih dan menganalisa aset kripto dengan{" "}
+                                <em>pair trading</em> USDT di <em>platform </em>
+                                perdagangan (Binance);
+                            </li>
+                            <li>
+                                Dapat melakukan open posisi pada semua aset
+                                kripto dengan <em>pair trading</em> USDT di{" "}
+                                <em>platform </em>perdagangan;
+                            </li>
+                            <li>
+                                Mengutamakan untuk melakukan <em>trading </em>
+                                pada <em>timeframe </em>15min dan/atau 1hour;
+                            </li>
+                            <li>
+                                Wajib melakukan <em>open position</em> minimal
+                                sebanyak 10-30 posisi (sesuai kondisi market)
+                                pada saat jadwal <em>shift</em>-nya;
+                            </li>
+                            <li>
+                                Wajib berhati-hati dalam melakukan{" "}
+                                <em>open position </em>terutama pada saat
+                                kondisi sedang <em>bearish</em> <em>market</em>;
+                            </li>
+                            <li>
+                                Hanya diperbolehkan mengambil profit trading
+                                minimal 1,5% dan maksimal 10%;
+                            </li>
+                            <li>
+                                Wajib mengetahui posisi support resisten dan
+                                dapat memperkirakan kondisi bearish market;
+                            </li>
+                            <li>
+                                Diperbolehkan melakukan <em>cut loss</em>{" "}
+                                apabila diperlukan (minimal 10% kerugian pada
+                                bitcoin dan diatas 15% pada altcoin) dengan
+                                kewajiban menyertakan alasan melakukan{" "}
+                                <em>cut loss</em> kepada Perusahaan melalui
+                                kolom isian pada <em>Dashboard Trader</em>;
+                            </li>
+                            <li>
+                                Wajib menginformasikan secara berkala kepada
+                                perusahaan terkait kondisi <em>market</em>; dan
+                            </li>
+                            <li>
+                                Wajib saling memberikan informasi dan saling
+                                berdiskusi terkait kondisi <em>market</em>.
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        Setiap <em>Trader </em>dilarang memberikan akses{" "}
+                        <em>backend</em> <em>trader</em> kepada siapapun dalam
+                        bentuk apapun
+                    </li>
+                    <li>
+                        Setiap <em>Trader </em>wajib menjaga semua informasi dan
+                        kerahasiaan segala aktivitas <em>trading </em>dan segala
+                        aktivitas terkait Perusahaan. Setiap <em>Trader </em>
+                        tidak diperkenankan untuk memberi tahu atau menyebabkan
+                        diketahuinya segala aktivitas <em>trading </em>dan
+                        aktivitas lain yang dijalankan Perusahaan kepada pihak
+                        manapun.
+                    </li>
+                </ol>
+                <p>&nbsp;</p>
+            </div>
         </div>
-      </div>
-      <div className="col-lg-12">
-        <p style={{ fontSize: "15px" }}>
-          Merupakan kumpulan peraturan yang dibuat untuk mempermudah tugas yang
-          dikerjakan. SOP merupakan tata cara atau tahapan yang dibukukan dan
-          yang harus dilalui trader untuk menyelesaikan suatu proses trading
-          tertentu. SOP adalah alat untuk mengurangi faktor kesalahan dan
-          ketidak disiplinan trader dalam melakukan proses trading.
-        </p>
-        <p style={{ fontSize: "16px", fontWeight: 500 }}>
-          Adapun SOP Metapro AI Trade adalah:
-        </p>
-        <p style={{ fontSize: "13px", fontWeight: 500 }}>
-          1. Mindset <br />
-          2. Time Management <br />
-          3. Money Management <br />
-          4. Preparation (power listrik/batterai PC/notebook,koneksi
-          internet,peralatan,situasi & kondisi yang baik) <br />
-          5. DOA sebelum memulai trading agar diberi HIKMAT dan KEBIJAKSANAAN
-          yang baik (spiritual) <br />
-          6. Day/Swing Trading <br />
-          7. Trend (mayot/long, minor/short) <br />
-          8. Support & Resistance (trendline, high low, price action, fibonacci.
-          indikator) <br />
-          9. Trigger/pemicu (T4,T3,T2,T1) <br />
-          10. Overbought / Oversold <br />
-          11. Entry Plan dalam bentuk Skenario Trading (If... Then... Else...){" "}
-          <br />
-          12. Risk & Reward (SL/TP) <br />
-          13. Cek News dan Baca Berita Fundamental Crypto <br />
-          14. Baca Analisa Pakar sebagai Konfirmasi & Komparasi saja <br />
-          15. Entry (Sell on High onn R kuat on Overbought, Buy on Low on S kuat
-          on Oversold) <br />
-          16. Input SL (wajib) & TP (tidak wajib) <br />
-          17. Recalculate (cut your losses let profits run)
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SOPTrader;
